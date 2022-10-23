bdb98223bbf8acb6feca031967d20d5db78755a7
Axel <eldrank@gmail.com>
2022-10-23 22:35:42 +0200

    feat: create taskArray to keep input value

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import Overview from './Overview';
 
 function App() {
+  const [taskArray, setTaskArray] = React.useState([]);
+
+  function handleChange(e) {
+    const newData = e.taget.value;
+    setTaskArray((prevData) => ({
+      ...prevData,
+      newData,
+    }));
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setTaskArray({
+      setTaskArray: taskArray.concat(taskArray),
+      taskArray: { text: '' },
+    });
+  }
+
   return (
     <div>
-      <h1>Hello</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={handleChange} />
+        <button type="submit">Send</button>
+      </form>
+      <Overview />
     </div>
   );
 }
---
+++ src/components/Overview.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+
+function Overview() {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+
+export default Overview;
