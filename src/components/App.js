41606c25f7d8c474825e54cfd052f26a23935288
Axel <eldrank@gmail.com>
2022-10-23 20:26:27 +0200

    feat: Make folder projet

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
